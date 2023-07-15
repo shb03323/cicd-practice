ac1e7fd567e0c9149ca2762c2bdf5c90fa12f907
jeonghoon <shb03323@naver.com>
2023-07-15 17:33:56 +0900

    feat: 다시 테스트 통과하도록 하기

--- src/test/java/practice/cicd/CrewTest.java
+++ src/test/java/practice/cicd/CrewTest.java
@@ -8,7 +8,7 @@ class CrewTest {
 
     @Test
     void 크루의_닉네임의_길이는_4까지다() {
-        assertThatThrownBy(() -> new Crew("옥상윤", "아디", 25))
+        assertThatThrownBy(() -> new Crew("옥상윤", "아디아디아디", 25))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 }
